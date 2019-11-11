829fddc45e54ad6e450cbdc77b241ebc14143d78
jmarra <jp.marra@mlb.com>
2019-11-11 11:58:48 -0500

    update manifest for pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,57 +1,57 @@
 module.exports = {
-    siteMetadata: {
-        title: `MLB News`,
-        description: `Statically generated legacy news articles`,
-        author: `@jpmarra`
+  siteMetadata: {
+    title: `MLB News`,
+    description: `Statically generated legacy news articles`,
+    author: `@jpmarra`
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-twitter`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-less`,
+    {
+      resolve: `gatsby-transformer-json`,
+      options: {
+        typeName: `Article`
+      }
     },
-    plugins: [
-        `gatsby-plugin-react-helmet`,
-        `gatsby-plugin-twitter`,
-        `gatsby-plugin-styled-components`,
-        `gatsby-plugin-less`,
-        {
-            resolve: `gatsby-transformer-json`,
-            options: {
-                typeName: `Article`
-            }
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                name: `articles`,
-                path: `${__dirname}/articles`
-            }
-        },
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        {
-            resolve: `gatsby-plugin-react-svg`,
-            options: {
-                rule: {
-                    include: /assets/
-                }
-            }
-        },
-        {
-            resolve: `gatsby-plugin-remote-images`,
-            options: {
-                nodeType: `Article`,
-                imagePath: `media.photo.cuts[0].src`
-            }
-        },
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
-                start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
-                display: `minimal-ui`
-                // icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
-            }
-        }, // this (optional) plugin enables Progressive Web App + Offline functionality
-        // To learn more, visit: https://gatsby.dev/offline
-        `gatsby-plugin-offline`
-    ]
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `articles`,
+        path: `${__dirname}/articles`
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-react-svg`,
+      options: {
+        rule: {
+          include: /assets/
+        }
+      }
+    },
+    {
+      resolve: `gatsby-plugin-remote-images`,
+      options: {
+        nodeType: `Article`,
+        imagePath: `media.photo.cuts[0].src`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `MLB News`,
+        short_name: `mlb-news`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#ececec`,
+        display: `minimal-ui`,
+        icon: `src/assets/logo.svg` // This path is relative to the root of the site.
+      }
+    }, // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    `gatsby-plugin-offline`
+  ]
 };
